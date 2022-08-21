c5408b9c05e3d828257b88613118e94079f610ce
709637 <vipul.winfo21@gmail.com>
2022-08-21 15:18:13 +0530

    testing

---
+++ home/site/wwwroot/precache-manifest.572cdf44161310cb5e4992bddd61ea09.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f9b704dd4a13506cb59e158da31e03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d6d99ef5d6d0dae7c6a",
+    "url": "/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "c584e0a2c88b15c87ce7",
+    "url": "/static/css/main.d4aae668.chunk.css"
+  },
+  {
+    "revision": "2d6d99ef5d6d0dae7c6a",
+    "url": "/static/js/2.d090776c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d090776c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565ff1b938f0251ffb4c",
+    "url": "/static/js/3.56d7ca26.chunk.js"
+  },
+  {
+    "revision": "ff6e3a6a0b259cdf071d",
+    "url": "/static/js/4.40748f80.chunk.js"
+  },
+  {
+    "revision": "c584e0a2c88b15c87ce7",
+    "url": "/static/js/main.cfba4ded.chunk.js"
+  },
+  {
+    "revision": "22d2a8dd547d4743c017",
+    "url": "/static/js/runtime-main.42b44ffe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
